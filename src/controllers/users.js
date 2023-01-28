039ba1ee779df445cf348a00f0f7c6d69473f5f7
Jelsafa <jelsafa.rb@gmail.com>
2023-01-28 17:33:20 -0300

    Fix Friends List

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -41,6 +41,9 @@ export const addRemoveFriend = async (req, res) => {
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
+    if (!user.friends) user.friends = [];
+    if (!friend.friends) friend.friends = [];
+
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((_id) => _id !== friendId);
       friend.friends = friend.friends.filter((_id) => _id !== id);
@@ -52,16 +55,12 @@ export const addRemoveFriend = async (req, res) => {
     await user.save();
     await friend.save();
 
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return {
-          _id,
-          firstName,
-          lastName,
-          occupation,
-          location,
-          picturePath,
-        };
+        return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
 
